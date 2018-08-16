2d397183de721b9bd27c055dab29033439323b1d
motsat <konpeiex@gmail.com>
2018-08-17 01:36:10 +0900

    user add sample

--- review.rb
+++ review.rb
@@ -8,13 +8,26 @@ module Ruboty
       require 'active_support/core_ext/string/filters'
       require 'redis-objects'
 
-      on(/review/i, name: "review", description: "assign review")
+      on(/review/i, name: "review", description: "review [group] [pull_request_url]")
 
 			def review(message)
-        team, pull_request_url = parse_message(message.body)
+        team, pull_request_url = parse_review_message(message.body)
         team_member_of(team: team)
         request_pull_request_review(pull_request_id_by(url: pull_request_url))
 				message.reply("<@#{message.original[:user]["id"]}> assigned!")
+			end
+
+      on(/useradd/i, name: "useradd", description: "useradd [slack_real_name or email] [github_account]")
+
+			def useradd(message)
+        slack_realname_or_email, github_account = parse_useradd_message(message.body)
+
+        slack_id = lack_id_by(realname_or_email: slack_realname_or_email)
+        if slack_id
+          message.reply("<@#{message.original[:user]["id"]}> found!")
+        else
+          message.reply("<@#{message.original[:user]["id"]}> not found!")
+        end
 			end
 
       private
@@ -32,7 +45,7 @@ module Ruboty
         users.find { |user| user[:teams].include? team }
       end
 
-      def parse_message(message_body)
+      def parse_review_message(message_body)
         vars = message_body.squish.split " "
         group, pull_request_url = 
           case vars.size
@@ -45,6 +58,11 @@ module Ruboty
           end
       end
 
+      def parse_useradd_message(message_body)
+        vars = message_body.squish.split " "
+        [vars[2], vars[3]] # 1.slack_realname or email, 2.github_account
+      end
+
       def pull_request_id_by(url:)
         url.split("/").last
       end
@@ -52,6 +70,26 @@ module Ruboty
       def request_pull_request_review(pull_request_id)
         octokit = Octokit::Client.new(access_token: ENV["OCTOKIT_ACCESS_TOKEN"])
         octokit.request_pull_request_review(ENV["GITHUB_REPOSITORY"], pull_request_id, reviewers: ["mo10sa10"])
+      end
+
+      def users_list
+        @users_list ||= slack_client.users_list
+      end
+
+      def slack_id_by(realname_or_email: )
+        users_list["members"].find do |member|
+          member_realname_or_emails = 
+            [member["profile"]["real_name"],
+             member["profile"]["real_name_normalized"],
+             member["profile"]["display_name"],
+             member["profile"]["email"]
+          ]
+          member_realname_or_emails.include? real_name_normalized
+        end
+      end
+
+      def slack_client
+        @slack_client ||= Slack::Client.new(token: ENV['SLACK_TOKEN'])
       end
 		end
 	end
