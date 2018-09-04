90b4ec3f05e640f5f99ec3d40b5607a03376c5e6
motsat <konpeiex@gmail.com>
2018-09-04 19:53:28 +0900

    fix commented reject

--- lib/ruboty/reviewer_assign/actions/assign.rb
+++ lib/ruboty/reviewer_assign/actions/assign.rb
@@ -16,8 +16,8 @@ module Ruboty
 
           reviewers = tag ?  Reviewer.by_tag(tag) : Reviewer.all
 
-          # assignしようとしている人がReviewer登録しているとも限らない
-          #reviewers.reject! {|r| r.slack_member_id == message.original[:user]["id"] }
+          # reject self
+          reviewers.reject! {|r| r.slack_member_id == message.original[:user]["id"] }
 
           return "<@#{message.original[:user]["id"]}> レビュアーが見つかりません:tired_face:" if reviewers.size == 0
           reviewer = reviewers.sort(&:last_reviewed_at).first
