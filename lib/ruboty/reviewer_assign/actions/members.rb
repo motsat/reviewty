f6a1816ba917a3d7fd01887ae44d544f1c990ef6
motsat <konpeiex@gmail.com>
2018-09-06 23:54:06 +0900

    update members descriptin

--- lib/ruboty/reviewer_assign/actions/members.rb
+++ lib/ruboty/reviewer_assign/actions/members.rb
@@ -14,9 +14,9 @@ module Ruboty
             Reviewer.all.map do |reviewer|
               slack_api = SlackAPI.new
               member = slack_api.find_member_by_id(reviewer.slack_member_id)
-              "@#{member["name"]}\n github: #{reviewer.github_account}\n tags: #{reviewer.tags.join "/"}\n last_reviewed_at: #{reviewer.last_reviewed_at}" 
+              "Slack @#{member["name"]}\n GitHub: #{reviewer.github_account}\n タグ: #{reviewer.tags.join "/"}\n 最終レビュー日時: #{reviewer.last_reviewed_at}" 
             end
-          "登録済のレビュアー:family:↓↓\n\n" + summarys.join("\n\n")
+          "レビュアー:family:↓↓\n\n" + summarys.join("\n\n")
         end
       end
     end
